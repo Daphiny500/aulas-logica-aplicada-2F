08f7bf26333a8724d1f0c076605d3d03cc7a7bc2
Daphiny500 <daphinyramos14@gmail.com>
2023-05-18 08:31:16 -0300

    aprendi sobre classes e objetos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Declarar objetos
+        Pessoa adao;
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+        //Acionar comportamento
+        adao.falar();
+        eva.falar();
     }
 }
